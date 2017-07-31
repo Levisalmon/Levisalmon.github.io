7d544078f57ffd2682f0f8d32825a3a2cc90ddbe
Levisalmon <30299639+Levisalmon@users.noreply.github.com>
2017-07-31 14:44:48 -0400

    Add files via upload

---
+++ js/controllers/MainController.js
@@ -0,0 +1,76 @@
+﻿app.controller('MainController', ['$scope', function ($scope) {
+    $scope.title = 'Portfolio Pieces';
+    $scope.pieces =
+        [
+            {
+                name: 'Lime Logo',
+                desc: 'A logo I designed for myself in photoshop',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/lime_port.jpg',
+                linkUrl: 'https://msu.edu/~salmonl1/portfolio/lime_port.jpg',
+                category: 'design'
+            },
+            {
+                name: 'Lime Logo',
+                desc: 'A logo I designed for myself in photoshop',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/lime_port.jpg',
+                linkUrl: 'https://msu.edu/~salmonl1/portfolio/lime_port.jpg',
+                category: 'design'
+            },
+            {
+                name: 'Lime Logo',
+                desc: 'A logo I designed for myself in photoshop',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/lime_port.jpg',
+                linkUrl: 'https://msu.edu/~salmonl1/portfolio/lime_port.jpg',
+                category: 'design'
+            },
+            {
+                name: 'Jungle Juice',
+                desc: 'A dance track I produced',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/jungle_port.jpg',
+                linkUrl: 'https://soundcloud.com/slayv-1/jungle-juice',
+                category: 'music'
+            },
+            {
+                name: "we don't talk anymore",
+                desc: 'A sample of a remix track',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/remix_port.jpg',
+                linkUrl: 'https://soundcloud.com/slayv-1/we-dont-talk-anymore-remix',
+                category: 'music'
+            },
+            {
+                name: 'ICCA performance',
+                desc: 'Performing Vocal Percussion in an A Capella set',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/pella_port.jpg',
+                linkUrl: 'https://www.youtube.com/watch?v=kG7KsqStrqY',
+                category: 'music'
+            },
+            {
+                name: "we don't talk anymore",
+                desc: 'A sample of a remix track',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/remix_port.jpg',
+                linkUrl: 'https://soundcloud.com/slayv-1/we-dont-talk-anymore-remix',
+                category: 'music'
+            },
+            {
+                name: 'ICCA performance',
+                desc: 'Performing Vocal Percussion in an A Caoekka set',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/pella_port.jpg',
+                linkUrl: 'https://www.youtube.com/watch?v=kG7KsqStrqY',
+                category: 'music'
+            },
+            {
+                name: 'Lime Logo',
+                desc: 'A logo I designed for myself in photoshop',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/lime_port.jpg',
+                linkUrl: 'https://msu.edu/~salmonl1/portfolio/lime_port.jpg',
+                category: 'design'
+            },
+            {
+                name: 'Jungle Juice',
+                desc: 'A dance track I produced',
+                photoUrl: 'https://msu.edu/~salmonl1/portfolio/jungle_port.jpg',
+                linkUrl: 'https://soundcloud.com/slayv-1/jungle-juice',
+                category: 'music'
+            }
+        ];
+}]);
