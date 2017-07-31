66e7958e0dbf7a7872ec989483672ca6fd8d7aba
Levisalmon <30299639+Levisalmon@users.noreply.github.com>
2017-07-31 14:42:14 -0400

    Add files via upload
    more folders

---
+++ Scripts/raindrops.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    document.getElementById("contain").onclick = function () {
+        var div = document.createElement('div');
+        var x = event.clientX;     // Get the horizontal coordinate
+        var y = event.clientY;
+        div.className = 'new-rect';// Get the vertical coordinate
+        div.style.left = event.clientX + 'px';
+        div.style.top = event.clientY + 'px';
+        document.getElementById("contain").appendChild(div);
+        setTimeout(function () {
+            $(".new-rect").remove();
+        }, 5000);
+    };
+};
